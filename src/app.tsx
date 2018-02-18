affab5fefe6a95b09c7a4839f1ad350de9b7348d
Daniel Ternyak <dternyak@gmail.com>
2018-02-18 04:52:03 -0600

    initial actual balance retrieval

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,7 @@ import * as React from 'react';
 import { Nav } from 'components/nav';
 import { Hero } from 'components/hero';
 import { Info } from 'components/info';
-import { store, RPCNode } from 'balancer';
 
-window.setTimeout(() => {
-  RPCNode('')
-    .getBalance('0xd47ca06c4866318ff9680695e39aa3a5bd337fbd')
-    .then(x => console.error(x.toString()));
-  console.error(store.getState());
-}, 2000);
 
 class App extends React.Component {
   render() {
