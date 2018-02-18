b83183839e8a66993f4af63534996b2e03544397
james-prado <jamesprado7@gmail.com>
2018-02-18 01:46:05 -0700

    Add hyz-shepherd

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import { Nav } from 'components/nav';
 import { Hero } from 'components/hero';
 import { Info } from 'components/info';
+import * as z from 'hyz-shepherd';
+
+window.setTimeout(() => {
+  console.error(z);
+  const node = RPCNode();
+  node.ping().then(x => console.log(x));
+}, 1000);
 
 class App extends React.Component {
   render() {
