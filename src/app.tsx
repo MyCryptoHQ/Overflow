f8e42eb2b8071a4e40e60dd9661e23d7ad6d5cc7
james-prado <jamesprado7@gmail.com>
2018-02-18 07:33:52 -0700

    Add footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Nav } from 'components/nav';
 import { Hero } from 'components/hero';
+import styled from 'styled-components';
+
+const Footer = styled.div`
+  padding: 32px;
+  font-size: 13px;
+  color: #aab8c5;
+`;
 
 class App extends React.Component {
   render() {
@@ -8,6 +15,7 @@ class App extends React.Component {
       <React.Fragment>
         <Nav />
         <Hero />
+        <Footer>© 2018 MyCrypto, Inc.</Footer>
       </React.Fragment>
     );
   }
