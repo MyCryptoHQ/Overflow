2b10707374135a245464d0891ac2bccbce4c747e
Will O'Beirne <wbobeirne@gmail.com>
2018-02-20 15:21:35 -0500

    Favicon

--- webpack_config/webpack.prod.js
+++ webpack_config/webpack.prod.js
@@ -1,8 +1,10 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const path = require('path');
 
 const config = merge(common, {
   output: {
@@ -33,7 +35,12 @@ const config = merge(common, {
     // new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.join(__dirname, '../src/assets/imgs/favicon.png'),
+      background: '#FFF',
+      inject: true
+    }),
   ]
 });
 
